cd2b162cf628289207876d309342a57389cdbf29
Ben Koo <koo0905@gmail.com>
2025-01-31 12:55:46 +0800

    Added the defineConfig reference to netlify URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-      site: "xlp.netlify.app"
+      site: "https://xlp.netlify.app"
 });
